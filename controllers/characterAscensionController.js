8cce16ccc0fd2e7f76468b4451bb5b14acbb292f
Jesus Urias <jesus.urias90@gmail.com>
2022-01-20 09:26:31 -0800

    Express server can now send the ascension materials of selected characters

--- controllers/characterAscensionController.js
+++ controllers/characterAscensionController.js
@@ -3,12 +3,26 @@ const {queryCharacterAscencionMaterials} = require('../database/queries')
 
 const getCharacterAscensionMaterials = async ( req,res)=>{
     try {
-        const results = await query(queryCharacterAscencionMaterials)
+        console.log(req.body)
+        if(!req.body.ids){
+            res.status(400).json({error:"Bad Request. ID's missing"})
+        }
+        const results = await query(queryCharacterAscencionMaterials(valueTemplateConstructor(req.body.ids))  , req.body.ids)
         res.status(200).json(results)
     }catch (e){
         console.log(e);
         res.status(500).json({error:"Internal Error"})
     }
+}
+
+const valueTemplateConstructor = (array) => {
+    let string = ""
+    for (let i = 0; i < array.length; i++) {
+        string += `$${i + 1}`
+        if (i >= array.length - 1) break
+        string += ","
+    }
+    return string
 }
 
 module.exports = {
