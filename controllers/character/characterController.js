9d480ca5c6643bb61eea3eeee4b524809b2a2362
Jesus Urias <dantails50@gmail.com>
2022-01-29 21:13:12 -0700

    backup data has been saved in the databse folder in its own location

--- controllers/character/characterController.js
+++ controllers/character/characterController.js
@@ -7,6 +7,7 @@ const allCharacters = async (req,res) =>{
 
         res.status(200).json(results?.rows ? { data: results.rows} : {data:[]})
     }catch (e){
+        console.log(e)
         res.status(500).json({error:"Internal Error"})
     }
 
